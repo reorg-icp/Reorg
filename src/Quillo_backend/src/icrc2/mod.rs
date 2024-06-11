8d4ca47ae1737fe057fd74936b0be9b8efe62714
amschel99 <6290.2020@students.ku.ac.ke>
2024-06-11 15:37:47 +0300

    Pass token generation argurments

--- src/Quillo_backend/src/icrc2/mod.rs
+++ src/Quillo_backend/src/icrc2/mod.rs
@@ -1,3 +1,5 @@
+use candid::CandidType;
+use candid::Encode;
 use candid::Principal;
 use ic_cdk::api::management_canister::main::{
     create_canister, install_code, CanisterSettings, CreateCanisterArgument, InstallCodeArgument,
@@ -11,31 +13,46 @@ use crate::ICRC1_LEDGER_WASM;
 use ic_cdk::trap;
 use ic_cdk_macros::{init, update};
 
-pub struct TokenDetailsPayload {
-    name: String,
-    symbol: String,
-    image: String,
-    minter: Principal,
-    transfer_fee: u128,
+#[derive(Serialize, Deserialize, CandidType)]
+struct MintingAccount {
+    owner: Principal,
 }
 
-pub struct TokenDetails {
-    name: String,
-    symbol: String,
-    image: String,
-    minter: Principal,
-    transfer_fee: u128,
-    pre_minted_tokens: u128,
-    archive_controller: Principal,
-    trigger_threshhold: u128,
-    blocks_to_archive: u128,
-    cycle_for_archive_creation: u128,
-    feature_flags: bool,
+#[derive(Serialize, Deserialize, CandidType)]
+struct MetadataRecord {
+    key: String,
+    value: MetadataValue,
+}
+
+#[derive(Serialize, Deserialize, CandidType)]
+#[serde(rename_all = "camelCase")]
+enum MetadataValue {
+    Text(String),
+}
+
+#[derive(Serialize, Deserialize, CandidType)]
+struct TokenDetails {
+    token_symbol: String,
+    token_name: String,
+    minting_account: MintingAccount,
+    transfer_fee: u64,
+    metadata: Vec<MetadataRecord>,
 }
 
 pub async fn create_and_deploy_canister() -> Result<String, TokenError> {
+    //example token
+    let my_token = TokenDetails {
+        token_name: "Reorg".to_string(),
+        token_symbol: "REO".to_string(),
+        transfer_fee: 1000,
+        minting_account: MintingAccount {
+            owner: Principal::from_text("aaaaa-aa").unwrap(), // Replace with a valid principal
+        },
+        metadata: vec![],
+    };
+    let serialized_args = Encode!(&my_token).expect("Serialization failed");
     let wasm_module = ICRC1_LEDGER_WASM.to_vec();
-    let cycles = 40_000_000_000_000;
+    let cycles = 8_093_982_275;
     let default_canister_settings: CanisterSettings = CanisterSettings::default();
 
     let create_args = CreateCanisterArgument {
@@ -50,9 +67,9 @@ pub async fn create_and_deploy_canister() -> Result<String, TokenError> {
 
     // Step 2: Install code to the new canister
     let install_args = InstallCodeArgument {
-        canister_id: new_canister_id.canister_id,
+        canister_id: new_canister_id.canister_id, //b77ix-eeaaa-aaaaa-qaada-cai
         wasm_module,
-        arg: vec![],
+        arg: serialized_args, // here is where we pass argurments for our token but serialized as bytes encoded in candid
         mode: ic_cdk::api::management_canister::main::CanisterInstallMode::Install,
     };
 
