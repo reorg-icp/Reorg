4bff24781ee3c4d8d617fe45ade566de24d85cbe
mburuanthony <mburuantony@outlook.com>
2024-06-17 12:48:25 +0300

    init ui

--- src/Quillo_frontend/vite.config.ts
+++ src/Quillo_frontend/vite.config.ts
@@ -1,9 +1,10 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import environment from "vite-plugin-environment";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath, URL } from "url";
+
 dotenv.config({ path: "../../.env" });
 
 export default defineConfig({
