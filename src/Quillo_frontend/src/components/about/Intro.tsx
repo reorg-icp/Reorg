fa3dcda801e486602fc87e550e5b571db1fc3b67
mburuanthony <mburuantony@outlook.com>
2024-06-26 10:34:26 +0300

    minor update

--- src/Quillo_frontend/src/components/about/Intro.tsx
+++ src/Quillo_frontend/src/components/about/Intro.tsx
@@ -5,20 +5,26 @@ export const Intro = (): JSX.Element => {
     <div className="reorgintro">
       <div className="forstartups">
         <p className="textLG">
-        Empowering Web3 Startups, dApps, and Protocols through Tokenization
-
+          Empowering Web3 Startups, dApps, and Protocols through Tokenization
         </p>
 
         <p className="desc">
-       At Reorg, we empower Web3 startups, including dApps, protocols, and DAOs, to seamlessly raise funds by leveraging the power of tokenization. Our platform simplifies the fundraising process, providing startups with the tools and support needed to tokenize their assets and attract global investors. 
+          At Reorg, we empower Web3 startups, including dApps, protocols, and
+          DAOs, to seamlessly raise funds by leveraging the power of
+          tokenization. Our platform simplifies the fundraising process,
+          providing startups with the tools and support needed to tokenize their
+          assets and attract global investors.
         </p>
       </div>
 
       <div className="forinvestors">
         <p className="textMD">for investors;</p>
         <p className="desc">
-        Discover, Invest, and Profit from Innovative Web3 Startups, dApps, and Protocols.
-At Reorg, we provide investors with unparalleled access to a diverse portfolio of Web3 startups, including dApps, protocols, and DAOs. Our platform enables you to discover promising projects, invest in tokenized assets, and participate in decentralized governance.
+          Discover, Invest, and Profit from Innovative Web3 Startups, dApps, and
+          Protocols. At Reorg, we provide investors with unparalleled access to
+          a diverse portfolio of Web3 startups, including dApps, protocols, and
+          DAOs. Our platform enables you to discover promising projects, invest
+          in tokenized assets, and participate in decentralized governance.
         </p>
       </div>
     </div>
