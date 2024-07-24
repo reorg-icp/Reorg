101908cbfdfac90cb0c0e956f8a3f6d09a75d9a6
amschel99 <6290.2020@students.ku.ac.ke>
2024-07-24 20:58:19 +0300

    add view tokens page

--- src/Quillo_frontend/src/pages/Tokens.tsx
+++ src/Quillo_frontend/src/pages/Tokens.tsx
@@ -79,7 +79,7 @@ const Tokens = () => {
         >
           <ol className="breadcrumb">
             <li className="breadcrumb-item">
-              <Link to="/">Tokens</Link>
+              <Link to="/">Home</Link>
             </li>
             <li className="breadcrumb-item active" aria-current="page">
               View Tokens
