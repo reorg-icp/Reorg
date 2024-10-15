e8c05ee07365f4ccea6e4eb6c797e55677cafef4
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-10-15 21:48:33 +0300

    pulled nft

--- setup.sh
+++ setup.sh
@@ -282,8 +282,4 @@ else
     # Write the text data to the file
     echo "$TEXT_DATA" > "$FILE_PATH"
     echo "File '$FILE_PATH' has been created and data has been written."
-<<<<<<< HEAD
 fi
-=======
-fi
->>>>>>> bb9048bf8ad9aca0b957ac9106cf3b271e5a1dfc
